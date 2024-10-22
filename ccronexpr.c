14c4ef1d21d291fb7a5f8cc14c87781dca6ddac6
Florian Knigge <flknigge@gmail.com>
2024-10-22 12:10:55 +0200

    Use `strcpy` instead of rewriting the whole field for 'H' replacement
    It requires tracking of the substring lens, but it should be more efficient with shorter writes.

--- ccronexpr.c
+++ ccronexpr.c
@@ -1350,6 +1350,7 @@ static char* check_and_replace_h(char* field, unsigned int pos, unsigned int min
                 return field;
             }
             size_t res_len = 0;
+            size_t res_lens[subfields_len];
             for (size_t i = 0; i < subfields_len; i++) {
                 has_h = strchr(subfields[i], 'H');
                 if (has_h) {
@@ -1359,7 +1360,8 @@ static char* check_and_replace_h(char* field, unsigned int pos, unsigned int min
                     free_splitted(subfields, subfields_len);
                     return field; // something went wrong trying to replace a subfield
                 }
-                res_len += strnlen(subfields[i], CRON_MAX_STR_LEN_TO_SPLIT);
+                res_lens[i] = strnlen(subfields[i], CRON_MAX_STR_LEN_TO_SPLIT);
+                res_len += res_lens[i];
             }
             // Allocate space for the full string: Result lengths + (result count - 1) for the commas + 1 for '\0'
             char *accum_field = (char *) cronMalloc(res_len + subfields_len );
@@ -1368,10 +1370,15 @@ static char* check_and_replace_h(char* field, unsigned int pos, unsigned int min
                 return field;
             }
             memset(accum_field, 0, res_len + subfields_len);
-            // Add initial field
-            sprintf(accum_field, "%s", subfields[0]);
-            for (size_t i = 1; i < subfields_len; i++) {
-                sprintf(accum_field, "%s,%s", accum_field, subfields[i]);
+            char *tracking = accum_field;
+            for (size_t i = 0; i < subfields_len; i++) {
+                strncpy(tracking, subfields[i], res_lens[i]);
+                tracking += res_lens[i];
+                // Don't append comma to last list entry
+                if (i < subfields_len-1) {
+                    strncpy(tracking, ",", 2); // using 2 to ensure the string ends in '\0', tracking will be set to that char
+                    tracking += 1;
+                }
             }
             free_splitted(subfields, subfields_len);
             cronFree(field);
